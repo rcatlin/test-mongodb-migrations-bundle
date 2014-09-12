ac540967f67396f8e8a0091a081affb7d3900ab5
Ryan Catlin <ryan.catlin@gmail.com>
2014-09-12 13:26:39 -0400

    Add monkey actions to DefaultController

--- src/TestMigrations/Bundle/MainBundle/Controller/DefaultController.php
+++ src/TestMigrations/Bundle/MainBundle/Controller/DefaultController.php
@@ -2,9 +2,11 @@
 
 namespace TestMigrations\Bundle\MainBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use TestMigrations\Bundle\MainBundle\Document\Monkey;
 
 class DefaultController extends Controller
 {
@@ -15,5 +17,58 @@ class DefaultController extends Controller
     public function indexAction($name)
     {
         return array('name' => $name);
+    }
+
+    /**
+     * @Route("/monkey/new/{name}")
+     */
+    public function newMonkeyAction($name)
+    {
+        $monkey = new Monkey();
+        $monkey->setName($name);
+
+        $dm = $this->getDocumentManager();
+        $dm->persist($monkey);
+        $dm->flush();
+
+        return new Response(
+            sprintf(
+                'Created Monkey %s',
+                $name
+            )
+        );
+    }
+
+    /**
+     * @Route("monkey/{name}")
+     */
+    public function findMonkeyAction($name)
+    {
+        $monkey = $this->getDocumentManager()
+            ->createQueryBuilder('TestMigrations\Bundle\MainBundle\Document\Monkey')
+            ->field('name')->equals($name)
+            ->getQuery()
+            ->getSingleResult()
+        ;
+
+        if ($monkey) {
+            $message = sprintf(
+                '%s is monkeying around. [%s]',
+                $monkey->getName(),
+                $monkey->getId()
+            );
+        } else {
+            $message = sprintf(
+                '%s is nowhere to be found.',
+                $name
+            );
+        }
+        
+        return new Response($message);
+    }
+
+    protected function getDocumentManager()
+    {
+        return $this->get('doctrine_mongodb')->getManager();
     }
 }
